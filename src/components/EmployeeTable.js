2853c431ce7a034df9c10bed438436dc825841f1
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-14 14:05:27 -0800

    Populate the employee table with the users data.

---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import TableHead from "./TableHead";
+import TableBody from "./TableBody";
+import Users from "../db/users.js";
+
+function EmployeeTable() {
+  const [users, setUsers] = useState({
+    users: []
+  });
+
+  useEffect(() => {
+    let data = Users.map(user => {
+      let {
+        name: { first: firstName, last: lastName },
+        email,
+        dob: { age },
+        phone: phoneNumber,
+        picture: { thumbnail }
+      } = user;
+      return { firstName, lastName, email, age, phoneNumber, thumbnail };
+    });
+    data.sort((a, b) => a.lastName.localeCompare(b.lastName));
+    setUsers({ users: data });
+  }, []);
+  return (
+    <table className="table">
+      <TableHead
+        colNames={[
+          "Last Name",
+          "First Name",
+          "Age",
+          "Email",
+          "Phone Number",
+          "Thumbnail"
+        ]}
+      />
+      <TableBody users={users} />
+    </table>
+  );
+}
+
+export default EmployeeTable;
