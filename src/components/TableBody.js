56bd7e365f36ce9ee56f2422e39e41d032994018
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-14 14:04:23 -0800

    Create the table body data from props data.

---
+++ src/components/TableBody.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function TableBody(props) {
+  const { users } = props.users;
+  const rowData = users.map(user => {
+    return (
+      <tr>
+        <td className="border-left border-right">{user.lastName}</td>
+        <td className="border-left border-right">{user.firstName}</td>
+        <td className="border-left border-right">{user.age}</td>
+        <td className="border-left border-right">{user.email}</td>
+        <td className="border-left border-right">{user.phoneNumber}</td>
+        <td className="border-left border-right">
+          <img
+            src={user.thumbnail}
+            alt={`${user.firstName} ${user.lastName}'s thumbnail.`}
+          />
+        </td>
+      </tr>
+    );
+  });
+  return <tbody>{rowData}</tbody>;
+}
+
+export default TableBody;
