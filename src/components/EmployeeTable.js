7ebbcf78985f473712f30dd83ac61d1e30dd9052
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-19 13:52:27 -0800

    Ability to sort table by header.

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,30 +1,97 @@
-import React, { useEffect, useState } from "react";
+import React, { useReducer } from "react";
 import TableHead from "./TableHead";
 import TableBody from "./TableBody";
-import Users from "../db/users.js";
 
-function EmployeeTable() {
-  const [users, setUsers] = useState({
-    users: []
+function EmployeeTable(props) {
+  let { users } = props;
+
+  const reducer = (state, action) => {
+    let usersNow = [...users];
+    if (action === state.type) {
+      switch (action) {
+        default:
+        case "Last Name":
+          if (state.order === "ASC") {
+            usersNow.sort((a, b) => b.lastName.localeCompare(a.lastName));
+          } else {
+            usersNow.sort((a, b) => a.lastName.localeCompare(b.lastName));
+          }
+          break;
+        case "First Name":
+          if (state.order === "ASC") {
+            usersNow.sort((a, b) => b.firstName.localeCompare(a.firstName));
+          } else {
+            usersNow.sort((a, b) => a.firstName.localeCompare(b.firstName));
+          }
+          break;
+        case "Email":
+          if (state.order === "ASC") {
+            usersNow.sort((a, b) => b.email.localeCompare(a.email));
+          } else {
+            usersNow.sort((a, b) => a.email.localeCompare(b.email));
+          }
+          break;
+        case "Age":
+          if (state.order === "ASC") {
+            usersNow.sort((a, b) => b.age - a.age);
+          } else {
+            usersNow.sort((a, b) => a.age - b.age);
+          }
+          break;
+        case "Phone Number":
+          if (state.order === "ASC") {
+            usersNow.sort(
+              (a, b) =>
+                b.phoneNumber.replace(/[() -]/gi, "") -
+                a.phoneNumber.replace(/[() -]/gi, "")
+            );
+          } else {
+            usersNow.sort(
+              (a, b) =>
+                a.phoneNumber.replace(/[() -]/gi, "") -
+                b.phoneNumber.replace(/[() -]/gi, "")
+            );
+          }
+          break;
+      }
+      users = usersNow;
+      return { ...state, order: state.order === "ASC" ? "DESC" : "ASC" };
+    } else {
+      switch (action) {
+        default:
+        case "Last Name":
+          usersNow.sort((a, b) => a.lastName.localeCompare(b.lastName));
+          break;
+        case "First Name":
+          usersNow.sort((a, b) => a.firstName.localeCompare(b.firstName));
+          break;
+        case "Email":
+          usersNow.sort((a, b) => a.email.localeCompare(b.email));
+          break;
+        case "Age":
+          usersNow.sort((a, b) => a.age - b.age);
+          break;
+        case "Phone Number":
+          usersNow.sort(
+            (a, b) =>
+              a.phoneNumber.replace(/[() -]/gi, "") -
+              b.phoneNumber.replace(/[() -]/gi, "")
+          );
+          break;
+      }
+      users = usersNow;
+
+      return { type: action, order: "ASC" };
+    }
+  };
+
+  const [sort, changeSortOrder] = useReducer(reducer, {
+    type: "Last Name",
+    order: "ASC"
   });
 
-  useEffect(() => {
-    let data = Users.map(user => {
-      let {
-        name: { first: firstName, last: lastName },
-        email,
-        login: { uuid: id },
-        dob: { age },
-        phone: phoneNumber,
-        picture: { thumbnail }
-      } = user;
-      return { firstName, lastName, email, id, age, phoneNumber, thumbnail };
-    });
-    data.sort((a, b) => a.lastName.localeCompare(b.lastName));
-    setUsers({ users: data });
-  }, []);
   return (
-    <table className="table">
+    <table className="table table-bordered">
       <TableHead
         colNames={[
           "Last Name",
@@ -32,8 +99,10 @@ function EmployeeTable() {
           "Age",
           "Email",
           "Phone Number",
-          "Thumbnail"
+          "Img"
         ]}
+        changeSortOrder={changeSortOrder}
+        sort={sort}
       />
       <TableBody users={users} />
     </table>
