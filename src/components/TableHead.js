d6d618708d714c05b384050a408b8b734d778f7e
Kami <51175640+hfurusho@users.noreply.github.com>
2020-02-19 13:55:23 -0800

    Functionality to sort by clicking headers.

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 function TableHead(props) {
-  const { colNames } = props;
+  const { colNames, changeSortOrder, sort } = props;
+  let icon;
+  if (sort.order === "ASC") {
+    icon = <i className="fas fa-sort-up"></i>;
+  } else {
+    icon = <i className="fas fa-sort-down"></i>;
+  }
   const tableHeaders = colNames.map(colName => (
-    <th scope="col" key={colName}>
-      {colName}
+    <th
+      scope="col"
+      key={colName}
+      onClick={colName !== "Img" ? () => changeSortOrder(colName) : () => {}}
+    >
+      {colName} {colName === sort.type ? icon : " "}
     </th>
   ));
 
   return (
-    <thead>
+    <thead class="thead-dark">
       <tr>{tableHeaders}</tr>
     </thead>
   );
